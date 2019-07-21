48971517c669cf05a7fef9f519664cd2e156ee81
Naman Datta <dattanaman213@gmail.com>
2019-07-21 10:42:49 +0530

    Add files via upload

---
+++ Naman_Datta/Day15/RFC.py
@@ -0,0 +1,68 @@
+#Data Preprocessing
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#import the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:,2:4].values
+Y = dataset.iloc[:,-1].values
+
+#splitting the dataset in training set and test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, Y_train, Y_test =  train_test_split(X, Y, test_size=0.2)
+
+#feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+X_train = sc_x.fit_transform(X_train)
+X_test = sc_x.transform(X_test)
+
+#fitting Classifier
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators=5, criterion="entropy")
+classifier.fit(X_train,Y_train)
+
+#predict
+y_pred = classifier.predict(X_test)
+
+#confusion matrix
+from sklearn.metrics import confusion_matrix
+cn_train = confusion_matrix(Y_train, classifier.predict(X_train))
+cn = confusion_matrix(Y_test,y_pred)
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, Y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('SVC (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, Y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('SVC (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
